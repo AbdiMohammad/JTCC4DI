8e777793f92232a53a177936c721fc4051bcdd30
AbdiMohammad <abdi.mo@northeastern.edu>
2024-03-12 16:09:01 -0400

    chore: add graceful continue

--- collect_data.py
+++ collect_data.py
@@ -18,7 +18,12 @@ if __name__ == '__main__':
             for funnel_prune_value in [60, 20, 40]:
                 json_data["funnel"]["prune_value"] = funnel_prune_value
                 output_folder = f"output/experiment_{experiment_id}"
-                pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+                try:
+                    pathlib.Path(output_folder).mkdir(parents=True)
+                except FileExistsError:
+                    print(f'PSNR: {PSNR}, Codebook Prune Value: {codebook_prune_value}, Funnel Prune Value: {funnel_prune_value} skipped')
+                    experiment_id += 1
+                    continue
                 json_data["output_folder"] = output_folder
                 with open(f"configs/experiment_{experiment_id}.json", 'w') as outfile:
                     json.dump(json_data, outfile, indent=4)
